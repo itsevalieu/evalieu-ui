79dd1a98847849cd560caf193d5686226a8ce556
Eva Lieu <itsevalieu@gmail.com>
2025-07-26 10:14:39 -0700

    #POR: add data samples

---
+++ portfolio/src/data/achievements.ts
@@ -0,0 +1,44 @@
+import { Achievement } from "@/types/achievement";
+
+export const achievements: Achievement[] = [
+  {
+    id: "1",
+    projectId: "ecommerce-platform",
+    title: "Created a repo",
+    date: "January 2025",
+    context:
+      "A full-featured e-commerce platform with product listings, shopping cart, and checkout functionality. This is testing how long it is. But alsdjflajsdflajds;flj",
+  },
+  {
+    id: "2",
+    projectId: "ecommerce-platform",
+    title: "Deployed the ecommerce platform",
+    date: "February 2024",
+    context:
+      "A full-featured e-commerce platform with product listings, shopping cart, and checkout functionality.",
+  },
+  {
+    id: "3",
+    projectId: "ecommerce-platform",
+    title: "Created a repo",
+    date: "March 2023",
+    context:
+      "A full-featured e-commerce platform with product listings, shopping cart, and checkout functionality.",
+  },
+  {
+    id: "4",
+    projectId: "ecommerce-platform",
+    title: "Deployed the ecommerce platform",
+    date: "January 2025",
+    context:
+      "A full-featured e-commerce platform with product listings, shopping cart, and checkout functionality.",
+  },
+  {
+    id: "5",
+    projectId: "ecommerce-platform",
+    title: "Deployed the ecommerce platform",
+    date: "September 2023",
+    context:
+      "A full-featured e-commerce platform with product listings, shopping cart, and checkout functionality.",
+  },
+];
